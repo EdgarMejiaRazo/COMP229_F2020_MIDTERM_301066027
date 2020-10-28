abd6a6782978e98d63df3a6d7d29495f9949e0ed
Edgar <edgaradrian.mejiarazo@gmail.com>
2020-10-27 21:22:34 -0400

    Getting rid of the folder

--- COMP229-F2020-MidTerm-301066027/server/config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb+srv://Edgar:rNzMfPvGt6nKjjzq@midtermf2020server.z5feb.mongodb.net/Book?retryWrites=true&w=majority"
-};
---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Edgar:rNzMfPvGt6nKjjzq@midtermf2020server.z5feb.mongodb.net/Book?retryWrites=true&w=majority"
+};
